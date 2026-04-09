6a3c7bccf600e3ec2ce762bf98f4c2f7fe695221
zakibn18 <jekben815@gmail.com>
2026-04-09 18:03:48 +0700

    methode untuk dataGridView

--- CRUDMahasiswaADO/Form1.cs
+++ CRUDMahasiswaADO/Form1.cs
@@ -228,5 +228,19 @@ namespace CRUDMahasiswaADO
                 MessageBox.Show("Terjadi kesalahan: " + ex.Message);
             }
         }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+                txtNIM.Text = row.Cells["NIM"].Value.ToString();
+                txtNama.Text = row.Cells["Nama"].Value.ToString();
+                cmbJK.Text = row.Cells["Jenis Kelamin"].Value.ToString();
+                dtpTanggalLahir.Value = Convert.ToDateTime(row.Cells["Tanggal Lahir"].Value);
+                txtAlamat.Text = row.Cells["Alamat"].Value.ToString();
+                txtKodeProdi.Text = row.Cells["Kode Prodi"].Value.ToString();
+            }
+        }
     }
 }
